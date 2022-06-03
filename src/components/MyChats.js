d43b156ea3d2b8b2b617bfb0063ce907d9c7d669
aditya <67224103+aditya-ahlawat-1309@users.noreply.github.com>
2022-06-03 14:56:40 +0530

    Update MyChats.js

--- src/components/MyChats.js
+++ src/components/MyChats.js
@@ -25,7 +25,7 @@ const MyChats = ({ fetchAgain }) => {
         },
       };
 
-      const { data } = await axios.get("/api/chat", config);
+      const { data } = await axios.get("https://chatstackapp.herokuapp.com/api/chat", config);
       setChats(data);
     } catch (error) {
       toast({
