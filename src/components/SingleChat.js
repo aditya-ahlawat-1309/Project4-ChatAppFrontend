3b20b07e5f69680a3aab8d065770bc1d453a7d6e
aditya <67224103+aditya-ahlawat-1309@users.noreply.github.com>
2022-06-03 14:57:27 +0530

    Update SingleChat.js

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -51,7 +51,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       setLoading(true);
 
       const { data } = await axios.get(
-        `/api/message/${selectedChat._id}`,
+        `https://chatstackapp.herokuapp.com/api/message/${selectedChat._id}`,
         config
       );
       setMessages(data);
@@ -82,7 +82,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
         };
         setNewMessage("");
         const { data } = await axios.post(
-          "/api/message",
+          "https://chatstackapp.herokuapp.com/api/message",
           {
             content: newMessage,
             chatId: selectedChat,
