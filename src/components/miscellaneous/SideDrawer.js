797e2a2a1ae8b34fd0cb33b3e54ef49b72b40f30
aditya <67224103+aditya-ahlawat-1309@users.noreply.github.com>
2022-06-03 14:54:59 +0530

    Update SideDrawer.js

--- src/components/miscellaneous/SideDrawer.js
+++ src/components/miscellaneous/SideDrawer.js
@@ -77,7 +77,7 @@ function SideDrawer() {
         },
       };
 
-      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      const { data } = await axios.get(`https://chatstackapp.herokuapp.com/api/user?search=${search}`, config);
 
       setLoading(false);
       setSearchResult(data);
@@ -104,7 +104,7 @@ function SideDrawer() {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.post(`/api/chat`, { userId }, config);
+      const { data } = await axios.post(`https://chatstackapp.herokuapp.com/api/chat`, { userId }, config);
 
       if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
       setSelectedChat(data);
