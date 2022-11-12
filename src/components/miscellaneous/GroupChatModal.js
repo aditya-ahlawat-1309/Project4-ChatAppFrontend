7343c25cc6b2daec3e9396798d2d624fb401ad22
Aditya Ahlawat <adityaroyal10@gmail.com>
2022-11-12 22:12:54 +0530

    Update GroupChatModal.js

--- src/components/miscellaneous/GroupChatModal.js
+++ src/components/miscellaneous/GroupChatModal.js
@@ -58,7 +58,7 @@ const GroupChatModal = ({ children }) => {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.get(`https://chatstackapp.herokuapp.com/api/user?search=${search}`, config);
+      const { data } = await axios.get(`https://lazy-pear-sea-lion-tam.cyclic.app/api/user?search=${search}`, config);
       console.log(data);
       setLoading(false);
       setSearchResult(data);
@@ -97,7 +97,7 @@ const GroupChatModal = ({ children }) => {
         },
       };
       const { data } = await axios.post(
-        `https://chatstackapp.herokuapp.com/api/chat/group`,
+        `https://lazy-pear-sea-lion-tam.cyclic.app/api/chat/group`,
         {
           name: groupChatName,
           users: JSON.stringify(selectedUsers.map((u) => u._id)),
